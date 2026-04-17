5ef86140371dc5d6c40de9584483ea7185f2ceab
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-17 15:16:28 +0530

    final commit

---
+++ react-server/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="background-glow"></div>
+      
+      <main className="content">
+        <h1 className="animated-text">
+          this react server is working
+        </h1>
+        
+        <div className="status-badge">
+          <span className="dot"></span>
+          System Operational
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
